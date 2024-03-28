5c9bd6206c3cfed362036c28f82e8f7bc8396b3d
Jens <klingaje@arcada.fi>
2024-03-28 15:34:44 +0200

    code challenge calcStdev

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,9 +22,10 @@ public class MainActivity extends AppCompatActivity {
 
     TextView outputCalculate;
 
+    double[] testdata = {658457, 297132, 244223, 239206, 209551, 195137, 144473, 121543, 120027, 83482, 80454, 77261, 72634, 67971, 67615, 64736, 64180, 52122, 51400, 51241, 51149, 47909, 46880, 45988, 45226, 38959, 37232, 36493, 35497, 34884, 33533, 32622, 32547, 28521, 27484, 25655, 24810, 24260, 23998, 21333, 20958, 20695, 20197, 19982, 19973, 19767, 19702, 19579, 19097, 18344, 18318, 17253, 16573, 16467, 15808, 15628, 15463, 15357, 15312, 15165, 15086, 14643, 14203, 12890, 12669, 12662, 12412, 11742, 11197, 11041, 10543, 10396, 10396, 9877, 9870, 9563, 9562, 9443, 9423, 9311, 9117, 8978, 8717, 8563, 8456, 7979, 7928, 7759, 7702, 7497, 7105, 7102, 6951, 6891, 6877, 6802, 6785, 6613, 6506, 6465, 6380, 6286, 6070, 5484, 5390, 4964, 1289};
+    ArrayList<Double> values = new ArrayList<>();
 
-
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint({"MissingInflatedId", "SetTextI18n"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,8 +39,14 @@ public class MainActivity extends AppCompatActivity {
         outputCalculate = findViewById(R.id.outputCalculate);
         outputCalculate.setText("Medeltalet är...");
 
+        // vi fyller vår arraylist med värdena från testdata-arrayen
+        for (double value : testdata) {
+            values.add(value);
+        }
 
     }
+
+
 
     public void buttonHandler(View view) {
         String text = inputText.getText().toString();
@@ -46,13 +54,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-
+    @SuppressLint("DefaultLocale")
     public void calculate(View view) {
-        double[] values = {3.5, 4.4, 9.0, 1.2, 6.9, 12.0};
 
-        outputCalculate.setText(String.format("Medeltalet är %.2f", Statistics.calcMean(values)));
-        outputText.setText(String.format("Medeltalet är %.2f", Statistics.calcMean(values)));
+        outputCalculate.setText(String.format(
+                "Medeltalet är: %.2f\n Median: %.2f\n standardavvikelse: %.2f",
+                Statistics.calcMean(values),
+                Statistics.calcMedian(values),
+                Statistics.calcStdev(values)
+        ));
+
+        outputText.setText(String.format(
+                "Medeltalet är: %.2f\n Median: %.2f\n standardavvikelse: %.2f",
+                Statistics.calcMean(values),
+                Statistics.calcMedian(values),
+                Statistics.calcStdev(values)
+        ));
     }
+
+
+
 
 }
--- app/src/main/java/fi/arcada/codechallenge/Statistics.java
+++ app/src/main/java/fi/arcada/codechallenge/Statistics.java
@@ -1,13 +1,43 @@
 package fi.arcada.codechallenge;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Statistics {
 
-    public static double calcMean(double[] values) {
+    public static double calcMean(ArrayList<Double> values) {
         double summa = 0;
-        for (int i = 0; i < values.length; i++) {
-            summa = summa + values[i];
+        // arraylist har .size() istället för length
+        for (int i = 0; i < values.size(); i++) {
+            summa = summa + values.get(i);
         }
 
-        return summa / values.length;
+        return summa / values.size();
     }
+
+    public static double calcMedian(ArrayList<Double> values) {
+        // vi skapar en kopia av values
+        ArrayList<Double> sorted = new ArrayList<>(values);
+        // sedan sorterar vi kopian. Annars sorteras ursprungliga datamängden
+        Collections.sort(sorted);
+
+        // detta funkar, ännu bättre skulle vara att kolla skillt för jämna datamängder
+        int midIndex = sorted.size()/2;
+
+        //median är det mittersta värdet i vår sorterade datamängd!
+        return sorted.get(midIndex);
+
+    }
+
+    public static double calcStdev(ArrayList<Double> values) {
+        double skillnad = 0;
+        for (int i = 0; i < values.size(); i++) {
+            skillnad = skillnad + Math.pow(values.get(i) - calcMean(values), 2);
+        }
+
+        double varians = skillnad/values.size();
+
+        return Math.sqrt(varians);
+    }
+
 }
