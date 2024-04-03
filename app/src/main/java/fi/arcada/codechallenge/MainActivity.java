cdd911f4c97f870629f938a420af6070e23c0c8c
Jens <klingaje@arcada.fi>
2024-04-03 13:18:06 +0300

    test

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -52,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     public void buttonHandler(View view) {
         String text = inputText.getText().toString();
         outputText.setText(text);
